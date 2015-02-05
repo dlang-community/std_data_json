fda6244d3dd2c66705c69f3d58913ac16e18d489
Matt Kline <matthew.kline@flukenetworks.com>
2015-02-05 11:58:53 -0800

    Add opIndex for JSONValue
    This allows you to index into an array or object without the
    .get!(JSONValue[]) boilerplate.

--- source/stdx/data/json/value.d
+++ source/stdx/data/json/value.d
@@ -37,6 +37,7 @@ struct JSONValue
 {
     @safe:
     import std.bigint;
+    import std.exception : enforce;
     import std.typecons : Nullable;
     import std.variant : Algebraic;
     import stdx.data.json.lexer : JSONToken;
@@ -111,6 +112,20 @@ struct JSONValue
         }
         return Nullable!JSONValue(cur);
     }
+
+    ref JSONValue opIndex(size_t idx)
+    {
+        auto asArray = payload.peek!(JSONValue[]);
+        enforce(asArray != null, "JSONValue is not an array");
+        return (*asArray)[idx];
+    }
+
+    ref JSONValue opIndex(string key)
+    {
+        auto asObject = payload.peek!(JSONValue[string]);
+        enforce(asObject != null, "JSONValue is not an object");
+        return (*asObject)[key];
+    }
 }
 
 /// Shows the basic construction and operations on JSON values.
@@ -126,14 +141,14 @@ unittest
     auto c = JSONValue([a, b]);
     assert(c.get!(JSONValue[])[0] == 12.0);
     assert(c.get!(JSONValue[])[1] == 13.0);
-    //assert(c[0] == 12);
-    //assert(c[1] == 13);
+    assert(c[0] == 12L);
+    assert(c[1] == 13L);
 
     auto d = JSONValue(["a": a, "b": b]);
     assert(d.get!(JSONValue[string])["a"] == 12.0);
     assert(d.get!(JSONValue[string])["b"] == 13.0);
-    //assert(d["a"] == 12);
-    //assert(d["b"] == 13);
+    assert(d["a"] == 12L);
+    assert(d["b"] == 13L);
 }
 
 /// Using $(D opt) to quickly access a descendant value.
